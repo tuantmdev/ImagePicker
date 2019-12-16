2e02329ddcac58e5d5d1e78fddae0d486a990be7
tuantm <tuantm.dev@gmail.com>
2019-12-16 23:32:28 +0700

    Fix issue with background queue

--- Source/CameraView/CameraMan.swift
+++ Source/CameraView/CameraMan.swift
@@ -175,7 +175,9 @@ class CameraMan {
                 }
                 
                 PHPhotoLibrary.shared().savePhoto(image: image, albumName: albumTitle, completion: { (_) in
-                    completion?()
+                    DispatchQueue.main.async {
+                        completion?()
+                    }
                 })
             }
         }
