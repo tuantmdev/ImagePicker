83923f66de1ed1c65484a36f2ee99b4973b44a61
anhdn <anhdn611@hotmail.com>
2020-03-14 11:40:25 +0700

    fix(CameraMan):Take image by device orientation

--- Source/CameraView/CameraMan.swift
+++ Source/CameraView/CameraMan.swift
@@ -180,7 +180,7 @@ class CameraMan {
     func takePhoto(_ previewLayer: AVCaptureVideoPreviewLayer, location: CLLocation?, completion: (() -> Void)? = nil) {
         guard let connection = stillImageOutput?.connection(with: AVMediaType.video) else { return }
         
-        connection.videoOrientation = Helper.getVideoOrientation(fromUIInferfaceOrientation: UIApplication.shared.statusBarOrientation)
+        connection.videoOrientation = Helper.getVideoOrientation(fromDeviceOrientation: UIDevice.current.orientation)
         
         queue.async {
             self.stillImageOutput?.captureStillImageAsynchronously(from: connection) { buffer, error in
